0daa73796cd0ae45a7877a1e3059fb5532c2af4a
Steffan Troxel <sjtroxel@alumni.iu.edu>
2025-03-15 22:02:01 -0500

    two-way binding on new-task form elements

--- 03-starting-project/src/app/user/user.component.ts
+++ 03-starting-project/src/app/user/user.component.ts
@@ -1,16 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-
-// type User = {
-//   id: string;
-//   avatar: string;
-//   name: string;
-// }
-
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
+import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -21,6 +10,7 @@ interface User {
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter();
 
 
